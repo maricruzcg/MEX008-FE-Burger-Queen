6af5869d3ea74bba549cfda5eda9602ef0321e6d
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-17 12:57:30 -0500

    Creando primer prueba navbar

--- burguerqueen/src/components/header.js
+++ burguerqueen/src/components/header.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 
 function Header(props) {
     // console.log(props);
     return (
-
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-                 Edit <code>src/App.js</code>
-                 {props.info}
+                {props.info}
+                {' '}
+                {props.dato}
             </p>
-            <p>
-                 {props.dato}
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-        </a>
         </header>
     );
 }

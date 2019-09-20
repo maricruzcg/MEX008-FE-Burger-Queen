f91b9bc05ade00c2945b3f0b8aa3da26fe491e0c
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-19 19:21:07 -0500

    Quitando pseudocodigo y corrigiendo texto de botón administracion

--- burguerqueen/src/components/Navbar.js
+++ burguerqueen/src/components/Navbar.js
@@ -7,13 +7,12 @@ class Navbar extends React.Component {
     }
     render() {
         return (<nav className={"navbar navbar-light bg-light"}>
-                <a className={"navbar-brand"} href="#">
-                <button type="button" className={"btn btn-dark"}>Ir a admisnistración</button>
+            <a className={"navbar-brand"} href="#">
+                <button type="button" className={"btn btn-dark"}>Ir a administración</button>
+            </a>
+        </nav>)
+    }
+};
 
-                </a>    
-            </nav>)
-            }
-        };
-        
-        Navbar = withRouter(Navbar);
-        export default Navbar;
+Navbar = withRouter(Navbar);
+export default Navbar;

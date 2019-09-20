e0de7c0e1ad49d9c8af263b087d2624ba8b0a20e
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-20 13:58:32 -0500

    Prueba con background image

--- burguerqueen/src/components/Background.js
+++ burguerqueen/src/components/Background.js
@@ -2,14 +2,9 @@ import React from 'react';
 import Image from '../imgFED/imagebackground.png';
 
 class Background extends React.Component {
-    getStyle = () => {
-        return {
-            backgroundImage: `url(${Image})`
-        }
-    }
     render() {
         return (
-            <img src={Image} />
+            <img className='backgroundImage' src={Image} />
         )
     }
 };

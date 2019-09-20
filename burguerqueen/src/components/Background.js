3186021538da4addda148b81b62a97ec6fdd80cf
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-09-20 15:10:00 -0500

    bajando cambios

--- burguerqueen/src/components/Background.js
+++ burguerqueen/src/components/Background.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Image from '../imgFED/imagebackground.png';
 
 class Background extends React.Component {
-    getStyle = () => {
-        return {
-            backgroundImage: `url(${Image})`
-        }
-    }
+    // getStyle = () => {
+    //     return {
+    //         backgroundImage: `url(${Image})`
+    //     }
+    // }
     render() {
         return (
-            <img src={Image} />
+            <img classsrc={Image} />
+            
         )
     }
 };

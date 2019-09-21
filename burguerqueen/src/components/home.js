fd01e905be717d9daf9669a6b58b5e9883df54c4
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-20 19:03:29 -0500

    Se tagea la versión. Acomodo de rutas y enlace a componentes

--- burguerqueen/src/components/home.js
+++ burguerqueen/src/components/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from "./Navbar";
 import Tips from "./Tips";
-import Username from "./Username"
+import Username from "./Username";
 //Se importan los componentes: 1) Boxtip 2)Boxmenu 3)Btn 4) tips 
 const Home = () => {
     return (<div>

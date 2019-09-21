630fa24871e7ce02959f871ced821e69c7d4c4c2
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-21 17:17:57 -0500

    Indentando tips.js y eliminando pseudocodigo didáctico

--- burguerqueen/src/pages/App.js
+++ burguerqueen/src/pages/App.js
@@ -21,13 +21,3 @@ class App extends Component {
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header info='Esto será la navbar.' dato='Ambos textos se enviaron cómo props desde el padre app.js.'/>
-//     </div>
-//   );
-// }
-
-// export default App;

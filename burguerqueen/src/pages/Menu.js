5d3c7face561ea72d4f13ba667bc8469f50c1f0c
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-09-22 02:46:53 -0500

    subiendo pruebas de btn icons

--- burguerqueen/src/pages/Menu.js
+++ burguerqueen/src/pages/Menu.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Nav from "../components/Navbar"
 import Boxfinish from "../components/Boxfinish"
+import Btn from "../components/Btn"
+
+import Food from "../imgFED/plate.png"
 
 const Menu = () => {
     return (
         <div>
             <Nav />
+            <Btn type="submit" class="menu-icon plate" text="Comida" />
+            <Btn type="submit" class="menu-icon" text="Bebidas" />
+            <Btn type="submit" class="menu-icon" text="Postres" />
             <Boxfinish />
         </div>
     );

2e6246ee31292e14614b6487e3a772c81ab478a6
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-09-22 23:39:31 -0500

    Se crea e imprime componente dropdowndrinks.js en page menu.js

--- burguerqueen/src/components/Dropdowndrinks.js
+++ burguerqueen/src/components/Dropdowndrinks.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import Btn from "./Btn";
 
-class Drinks extends React.Component {
-    render() {
-        return (
-
-        )
+export default class Example extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        dropdownOpen: false
+      };
     }
-};
-
-export default Drinks;
+  
+    toggle() {
+      this.setState({
+        dropdownOpen: !this.state.dropdownOpen
+      });
+    }
+  
+    render() {
+      return (
+        <ButtonDropdown className={"menu-icon"} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle caret>
+              <Btn text="MALTEADAS" className="btn btn-secondary" />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem><Btn text="FRESA" className="btn btn-secondary" /></DropdownItem>
+            <DropdownItem><Btn text="MAZAPÁN" className="btn btn-secondary" /></DropdownItem>
+            <DropdownItem><Btn text="CHOCOMENTA"  /></DropdownItem>
+            <DropdownItem><Btn text="OTRO" className="btn btn-secondary" /></DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+      );
+    }
+  }

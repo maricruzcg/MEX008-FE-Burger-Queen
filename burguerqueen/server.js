f3211fbca639c319fa1ad09a6599ae9199a25403
eriika19 <enciso.iq@gmail.com>
2019-09-26 18:44:16 -0500

    se inicializo otro router con mongo y express

--- burguerqueen/server.js
+++ burguerqueen/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
+const connectDB = require('./config/db')
+
 const app = express();
 
+//Connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/orders', require('./routes/api/orders'));
 
 const PORT = process.env.PORT || 5000;
 

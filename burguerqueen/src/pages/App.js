bf29507c86547902f1192adca03bb512f0a0e6aa
eriika19 <enciso.iq@gmail.com>
2019-09-30 17:52:30 -0500

    ultimos cambios 300919

--- burguerqueen/src/pages/App.js
+++ burguerqueen/src/pages/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Menu from '../pages/Menu';
 import './App.css';
@@ -8,10 +8,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Route path="/Home" component={Home} />
             <Route path="/Menu" component={Menu} />
-        </BrowserRouter>
+        </HashRouter>
     );
   }
 }

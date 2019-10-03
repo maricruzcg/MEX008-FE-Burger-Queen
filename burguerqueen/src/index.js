65b7fecd36db34be1f1ff3e02a361cccf30db4b7
eriika19 <51388362+eriika19@users.noreply.github.com>
2019-10-03 00:37:28 -0500

    segunda prueba para deploy

--- burguerqueen/src/index.js
+++ burguerqueen/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render((
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>
+    </HashRouter>
 ), document.getElementById('root'));
 
 serviceWorker.register();

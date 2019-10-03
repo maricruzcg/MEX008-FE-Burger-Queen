7cddd9d5f42ecddd134352a632ac5658857efc39
Laboratoria008 <jessicazetina37@gmail.com>
2019-10-02 21:08:30 -0500

    agregando pantalla de cocina

--- burguerqueen/src/pages/App.js
+++ burguerqueen/src/pages/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Menu from '../pages/Menu';
+import Login from '../pages/Login';
 import './App.css';
 
 
@@ -11,6 +12,7 @@ class App extends Component {
         <HashRouter>
             <Route path="/Home" component={Home} />
             <Route path="/Menu" component={Menu} />
+            <Route path="/Login" component={Login}/>
         </HashRouter>
     );
   }

1f165adabe2a36aaace588f0aee580550031a918
eriika19 <enciso.iq@gmail.com>
2019-10-05 13:11:57 -0500

    se dio mas estilo y se arreglaron conflictos

--- burguerqueen/src/pages/App.js
+++ burguerqueen/src/pages/App.js
@@ -14,11 +14,11 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
+          <Route exact path={process.env.PUBLIC_URL + '/Getin'} component={GetIn} />
           <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
           <Route path={process.env.PUBLIC_URL + '/Menu'} component={Menu} />
-          <Route path={process.env.PUBLIC_URL + '/Login'} component={Login} />
-          <Route path={process.env.PUBLIC_URL + '/Getin'} component={GetIn} />
           <Route path={process.env.PUBLIC_URL + '/Kitchen'} component={Kitchen} />
+          <Route path={process.env.PUBLIC_URL + '/Login'} component={Login} />
 
           <Route path={process.env.PUBLIC_URL + '*'} component={Error} />
         </Switch>

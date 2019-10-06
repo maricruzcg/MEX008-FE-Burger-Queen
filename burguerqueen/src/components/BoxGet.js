abe41e582ca43d3516e09c72232b3a5b4a600b84
ViLaRa <vianey.lara.rangel@gmail.com>
2019-10-05 19:28:55 -0500

    Fusinnn exitosa, se actualizaron dependencias con npm i npm-check-updates

--- burguerqueen/src/components/BoxGet.js
+++ burguerqueen/src/components/BoxGet.js
@@ -1,85 +1,56 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Button } from 'reactstrap';
+  import React from 'react';
+  import { withRouter } from 'react-router-dom';
+  // import { Button } from 'reactstrap';
+  // import logo from '../imgFED/logo.png';
 
-import logo from '../imgFED/logo.png';
+  class BoxGet extends React.Component {
+    handleClick(ev) {
+      ev.preventDefault();
+      this.props.history.push('/Home');
+    }
+    
+    render() {
 
-class BoxGet extends React.Component {
+        return (
+            <section className={'box-password'}>
+              <section className={'box-center'}>
+                <h3>
+                  <strong>Ingresa tu contraseña</strong>
+                </h3>
 
-  handleClick(ev) {
-    ev.preventDefault();
-    this.props.history.push('/Home');
+                <input type="password" className={'input-password'}></input>
+                <div className={'box-buttons'}>
+                  <div>
+                    <input className={'btn-number'} type="button" value="7"></input>
+                    <input className={'btn-number'} type="button" value="8"></input>
+                    <input className={'btn-number'} type="button" value="9"></input>
+                  </div>
+
+                  <div>
+                    <input className={'btn-number'} type="button" value="4"></input>
+                    <input className={'btn-number'} type="button" value="5"></input>
+                    <input className={'btn-number'} type="button" value="6"></input>
+                  </div>
+
+                  <div>
+                    <input className={'btn-number'} type="button" value="1"></input>
+                    <input className={'btn-number'} type="button" value="2"></input>
+                    <input className={'btn-number'} type="button" value="3"></input>
+                  </div>
+
+                  <div>
+                    <input className={'btn-number'} type="button" value="<"></input>
+                    <input className={'btn-number'} type="button" value="0"></input>
+                    <input className={'btn-number'} type="button" value="C"></input>
+                  </div>
+                </div>
+
+                <button className={'btn-valid-password'} onClick={this.handleClick.bind(this)} >Flecha</button>
+              </section>
+            </section>
+        );
+    }
   }
+
+  export default withRouter(BoxGet);
   
-  render() {
-
-    return (
-      <section className={'form-in'}>
-        <section className={'box-welcome'}>
-          <img className={'img-log'} src={logo} alt="logo" />
-             <h3><strong>Ingresa tu contraseña</strong></h3>
-              
-              <input type="text" className="input-password"></input>
-              
-                  <div className="box-buttons">
-                    <div>
-                      <input className="btn-number" type="button" value="7"></input>
-                      <input className="btn-number" type="button" value="8"></input>
-                      <input className="btn-number" type="button" value="9"></input>
-                    </div>
-          <div className={'text-welcome'}>
-            <h2>¡TE DAMOS LA </h2>
-            <h2>BIENVENIDA!</h2>
-          </div>
-          <div className={'box-btn-welcome'}>
-            <Button className={'btn-login'} color="secondary">
-              <h4>EQUIPO</h4>
-            </Button>
-            <Button className={'btn-login'} color="secondary">
-              <h4>ADMINISTRACIÓN</h4>
-            </Button>
-          </div>
-        </section>
-
-        <section className={'box-password'}>
-          <section className={'box-center'}>
-            <h3>
-              <strong>Ingresa tu contraseña</strong>
-            </h3>
-
-            <input type="password" className={'input-password'}></input>
-            <div className={'box-buttons'}>
-              <div>
-                <input className={'btn-number'} type="button" value="7"></input>
-                <input className={'btn-number'} type="button" value="8"></input>
-                <input className={'btn-number'} type="button" value="9"></input>
-              </div>
-
-              <div>
-                <input className={'btn-number'} type="button" value="4"></input>
-                <input className={'btn-number'} type="button" value="5"></input>
-                <input className={'btn-number'} type="button" value="6"></input>
-              </div>
-
-              <div>
-                <input className={'btn-number'} type="button" value="1"></input>
-                <input className={'btn-number'} type="button" value="2"></input>
-                <input className={'btn-number'} type="button" value="3"></input>
-              </div>
-
-              <div>
-                <input className={'btn-number'} type="button" value="<"></input>
-                <input className={'btn-number'} type="button" value="0"></input>
-                <input className={'btn-number'} type="button" value="C"></input>
-              </div>
-            </div>
-
-            <button className={'btn-valid-password'} onClick={this.handleClick.bind(this)} >Flecha</button>
-          </section>
-        </section>
-      </section>
-    );
-  }
-}
-
-export default withRouter(BoxGet);
